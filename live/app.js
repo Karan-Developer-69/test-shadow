f78f0cee63c5f7638fded5d487821fcca7bb4031
Karan <homepc8890@gmail.com>
2026-01-15 21:54:56 +0530

    Update app.js

--- live/app.js
+++ live/app.js
@@ -1,8 +1,8 @@
 const Shadow = require('shadow-deploy-client');
 
 const shadow = new Shadow({
-  apiKey: 'sk_5e03iby11',
-  projectId: 'proj_p0y19ec9j'
+  apiKey: 'sk_owmyusqkd',
+  projectId: 'proj_h30awkbij'
 });
 
 require('dotenv').config();
